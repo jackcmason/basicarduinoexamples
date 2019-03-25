e7c920eb86b645b70de95704de69a71b4e68d4ce
Jack Mason <jack.mason@posteo.net>
2019-03-25 13:13:28 +1000

    comment fading LED program

--- buttons/fadebutton/fadebutton.ino
+++ buttons/fadebutton/fadebutton.ino
@@ -1,25 +1,33 @@
-int pressed = 0;
-int ledon = 0;
-unsigned long fadetime = 0;
+int pressed = 0; //is the button pressed? 0 is no, 1 is yes
+int ledon = 0; //is the LED on? 0 is no, 1 is yes
+unsigned long fadetime = 0; //what time was the LED toggled? (ms since program start)
+int buttonPin = 2; //Arduino pin button is connected to
+int ledPin = 11; //Arduino pin LED is connected to (needs to be a PWM pin)
 
-void setup() {
-  pinMode(2, INPUT);
-  pinMode(11, OUTPUT);
+void setup() { //program setup
+  pinMode(buttonPin, INPUT); //button is an input
+  pinMode(ledPin, OUTPUT); //button is an output
   delay(300);
 }
 
 void loop() {
-  if(digitalRead(2) == HIGH && !pressed) {
-    pressed = 1;
-    ledon = !ledon;
-    fadetime = millis();
+  //if button was pressed and was not previously pressed
+  if(digitalRead(buttonPin) == HIGH && !pressed) {
+    pressed = 1; //button was pressed
+    ledon = !ledon; //toggle LED
+    fadetime = millis(); //store time
   }
-  if((digitalRead(2) == LOW) && pressed) {
-    pressed = 0;
+  if((digitalRead(2) == LOW) && pressed) {//if button is not pressed and was pressed previously
+    pressed = 0; //button is not pressed
   }
-  fadefunction(ledon, millis()-fadetime, 11, 100);
+  fadefunction(ledon, millis()-fadetime, 11, 100); //adjust LED brightness
 }
 
+//function which handles LED fading
+//takes led state to determine if it is fading on or off
+//takes time since led toggle to determine which part of the "fade cycle" is occuring
+//takes pin number (LED pin) to determine which pin to act on
+//takes a duration to determine how long each phase of the fade should last
 int fadefunction(int led, unsigned long starttime, int pin, int inc) {
   if ((starttime > 0 && starttime < 1*inc && led) || (starttime > 5*inc && !led)) {
     analogWrite(pin, 0);

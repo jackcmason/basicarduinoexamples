c71f3aa524ffcd8cb56902b15bcf958eb218649f
Jack Mason <jack.mason@posteo.net>
2019-03-25 13:00:45 +1000

    commented strobe led program

--- buttons/strobebutton/strobebutton.ino
+++ buttons/strobebutton/strobebutton.ino
@@ -1,18 +1,24 @@
-void setup() {
-  pinMode(2, INPUT);
-  pinMode(13, OUTPUT);
+//program which flashes LED when
+//button is pressed
+
+int buttonPin = 2; //Arduino pin where button is attached
+int ledPin = 13; //Arduino pin where LED is attached
+
+void setup() { //program setup
+  pinMode(buttonPin, INPUT); //button is an input
+  pinMode(ledPin, OUTPUT); //LED is an output
 
 }
 
 void loop() {
-  if (digitalRead(2) == HIGH) {
-    if((millis() % 125) < 63) {
+  if (digitalRead(buttonPin) == HIGH) { //if button is pressed
+    if((millis() % 125) < 63) { //LED on and off in controlled intervals
       digitalWrite(13, HIGH);
     } else {
       digitalWrite(13, LOW);
     }
   } else {
-    digitalWrite(13, LOW);
+    digitalWrite(13, LOW); //LED off
   }
 
 }

c7bd6bc33eefd5386a109376d9b60f516f41bbae
Jack Mason <jack.mason@posteo.net>
2019-03-25 14:51:30 +1000

    more fade changes

--- buttons/fadebutton/fadebutton.ino
+++ buttons/fadebutton/fadebutton.ino
@@ -12,15 +12,15 @@ void setup() { //program setup
 
 void loop() {
   //if button was pressed and was not previously pressed
-  if(digitalRead(buttonPin) == HIGH && !pressed) {
+  if (digitalRead(buttonPin) == HIGH && !pressed) {
     pressed = 1; //button was pressed
     ledon = !ledon; //toggle LED
     fadetime = millis(); //store time
   }
-  if((digitalRead(2) == LOW) && pressed) {//if button is not pressed and was pressed previously
+  if ((digitalRead(2) == LOW) && pressed) { //if button is not pressed and was pressed previously
     pressed = 0; //button is not pressed
   }
-  fadefunction(ledon, millis()-fadetime, 11, 100); //adjust LED brightness
+  fadefunction(ledon, millis() - fadetime, 11, 100); //adjust LED brightness
 }
 
 //function which handles LED fading
@@ -29,22 +29,22 @@ void loop() {
 //takes pin number (LED pin) to determine which pin to act on
 //takes a duration to determine how long each phase of the fade should last
 int fadefunction(int led, unsigned long starttime, int pin, int inc) {
-  if ((starttime > 0 && starttime < 1*inc && led) || (starttime > 5*inc && !led)) {
+  if ((starttime > 0 && starttime < 1 * inc && led) || (starttime > 5 * inc && !led)) {
     analogWrite(pin, 0);
   }
-  if ((starttime > 1*inc && starttime < 2*inc && led) || (starttime > 4*inc && starttime < 5*inc && !led)) {
+  if ((starttime > 1 * inc && starttime < 2 * inc && led) || (starttime > 4 * inc && starttime < 5 * inc && !led)) {
     analogWrite(pin, 50);
   }
-  if ((starttime > 2*inc && starttime < 3*inc && led) || (starttime > 3*inc && starttime < 4*inc && !led)) {
+  if ((starttime > 2 * inc && starttime < 3 * inc && led) || (starttime > 3 * inc && starttime < 4 * inc && !led)) {
     analogWrite(pin, 100);
   }
-  if ((starttime > 3*inc && starttime < 4*inc && led) || (starttime > 2*inc && starttime < 3*inc && !led)) {
+  if ((starttime > 3 * inc && starttime < 4 * inc && led) || (starttime > 2 * inc && starttime < 3 * inc && !led)) {
     analogWrite(pin, 150);
   }
-  if ((starttime > 4*inc && starttime < 5*inc && led) || (starttime > 1*inc && starttime < 2*inc && !led)) {
+  if ((starttime > 4 * inc && starttime < 5 * inc && led) || (starttime > 1 * inc && starttime < 2 * inc && !led)) {
     analogWrite(pin, 200);
   }
-  if ((starttime > 5*inc && led) || (starttime > 0 && starttime < 1*inc && !led)) {
+  if ((starttime > 5 * inc && led) || (starttime > 0 && starttime < 1 * inc && !led)) {
     analogWrite(pin, 255);
   }
   return 0;

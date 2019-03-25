ff4bd2ceacd7d2c9c55d32e917c946fbb07d4274
Jack Mason <jack.mason@posteo.net>
2019-03-25 12:55:26 +1000

    added comments for inverse button program

--- buttons/inversesoftwarebutton/inversesoftwarebutton.ino
+++ buttons/inversesoftwarebutton/inversesoftwarebutton.ino
@@ -1,14 +1,20 @@
-void setup() {
-  pinMode(2, INPUT);
-  pinMode(13, OUTPUT);
+//Progam for an LED to turn off
+//when a button is pressed
+
+int buttonPin = 2;
+int ledPin = 13;
+
+void setup() { //program setup
+  pinMode(buttonPin, INPUT); //button is an input
+  pinMode(ledPin, OUTPUT); //LED is an output
 
 }
 
-void loop() {
-  if (digitalRead(2) == HIGH) {
-    digitalWrite(13, LOW);
+void loop() { //begin program
+  if (digitalRead(buttonPin) == HIGH) { //if button is pressed
+    digitalWrite(ledPin, LOW); //LED off
   } else {
-    digitalWrite(13, HIGH);
+    digitalWrite(13, HIGH); //LED on
   }
 
 }

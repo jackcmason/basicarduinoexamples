a552ff6390372cc1e8f45c6c0726cc125b483455
Jack Mason <jack.mason@posteo.net>
2019-03-25 10:17:32 +1000

    commenting on software button program

--- buttons/softwarebutton/softwarebutton.ino
+++ buttons/softwarebutton/softwarebutton.ino
@@ -1,14 +1,19 @@
-void setup() {
-  pinMode(2, INPUT);
-  pinMode(13, OUTPUT);
+//Use Programming to control an LED rather than having an LED 
+//in series with a switch
+
+int buttonPin = 2; //Arduino pin where the button is connected
+int ledPin = 13; //Arduino pin where the LED is connected
+
+void setup() { //setup pins
+  pinMode(buttonPin, INPUT); //button is an input
+  pinMode(ledPin, OUTPUT); //LED is an output
 
 }
 
-void loop() {
-  if (digitalRead(2) == HIGH) {
-    digitalWrite(13, HIGH);
+void loop() { //run program
+  if (digitalRead(buttonPin) == HIGH) { //if the button is pressed
+    digitalWrite(ledPin, HIGH); //LED on
   } else {
-    digitalWrite(13, LOW);
+    digitalWrite(ledPin, LOW); //LED off
   }
-
 }
